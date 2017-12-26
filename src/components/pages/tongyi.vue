24b8e3e8196c5f706f23f936aaf263f704cfb681
Smooth Catfish <30784411+Smooth-Catfish@users.noreply.github.com>
2017-12-26 21:23:41 +1100

    fiddled - echo, spectre, tongyi, update skeletons

---
+++ src/components/pages/tongyi.vue
@@ -0,0 +1,44 @@
+<!-- Template -->
+<template>
+    <div id="" class="content">
+
+    </div>
+</template>
+
+<!-- Scripts -->
+<script>
+    export default {
+        data () {
+            return {
+                home: {
+                    identifier: 'Tongyi',
+                    title: 'Tongyi Shen',
+                    author: '',
+                    content: '',
+                    icon: 'fa-user',
+                    has_own_component: true,
+                    active: true,
+                    primary_page: true,
+                }, // Home
+            }
+        },
+
+        props: ['page', 'pages'],
+
+        methods: {
+            setPage: function(page){
+                this.$emit('page', page)
+                //alert('hey')
+            }
+        },
+
+        components: {
+
+        }
+    }
+</script>
+
+<!-- Style -->
+<style scoped>
+
+</style>
